7379160fbed2a77df911fbb0d85e4f1b6b44b302
s4murai <gerasimov.vladislav@gmail.com>
2016-10-23 12:45:06 +0300

    Fix indentations in .cpp

--- src/fttg_daemon.cpp
+++ src/fttg_daemon.cpp
@@ -11,26 +11,26 @@ using namespace std;
 const string command = "python ../src/fttg.py";
 
 void logger(const string& text) {
-	ofstream log("../log.txt", ios::app);
-	time_t now = time(0);
-	string dt = ctime(&now);
-	log << dt << " " << text << endl;
-	log.close();
+    ofstream log("../log.txt", ios::app);
+    time_t now = time(0);
+    string dt = ctime(&now);
+    log << dt << " " << text << endl;
+    log.close();
 }
 
 void process() {
-	// Create a fork which will continue to run main loop
-	if (pid_t pid = fork()) {
-		// Launch command in parent process and exit
-		if (pid > 0) {
-			logger("Launching python script");
-			execl("/bin/sh", "sh", "-c", command.c_str(), (char *) 0);
-			exit(0);
-		}
-		else if (pid < 0) {
-			logger("Failed to launch python script");
-		}
-	}
+    // Create a fork which will continue to run main loop
+    if (pid_t pid = fork()) {
+        // Launch command in parent process and exit
+        if (pid > 0) {
+            logger("Launching python script");
+            execl("/bin/sh", "sh", "-c", command.c_str(), (char *) 0);
+            exit(0);
+        }
+        else if (pid < 0) {
+            logger("Failed to launch python script");
+        }
+    }
 }
 
 int daemonize() {
@@ -39,20 +39,20 @@ int daemonize() {
     // also a prerequisite for the subsequent call to setsid().
     if (pid_t pid = fork())
     {
-      if (pid > 0)
-      {
-        // We're in the parent process and need to exit.
-        //
-        // When the exit() function is used, the program terminates without
-        // invoking local variables' destructors. Only global variables are
-        // destroyed.
-        exit(0);
-      }
-      else
-      {
-        logger("First fork failed");
-        return 1;
-      }
+        if (pid > 0)
+        {
+            // We're in the parent process and need to exit.
+            //
+            // When the exit() function is used, the program terminates without
+            // invoking local variables' destructors. Only global variables are
+            // destroyed.
+            exit(0);
+        }
+        else
+        {
+            logger("First fork failed");
+            return 1;
+        }
     }
 
     // The file mode creation mask is also inherited from the parent process.
@@ -67,12 +67,12 @@ int daemonize() {
     logger("Creating new signature for the fork, PID " + to_string(sid));
     
     if (sid < 0) {
-		logger("Signature creation failed");
-		exit(EXIT_FAILURE); 
-	}
+        logger("Signature creation failed");
+        exit(EXIT_FAILURE); 
+    }
 
-/*  -- Not using this because python script should be in the same
-	directory as the daemon --
+    /*  -- Not using this because python script should be in the same
+    directory as the daemon --
     
     // A process inherits its working directory from its parent. This could be
     // on a mounted filesystem, which means that the running daemon would
@@ -82,23 +82,23 @@ int daemonize() {
     logger("Changing directory");
     
     if ((chdir("/")) < 0) {
-		logger("Failed to change directory");
-		exit(EXIT_FAILURE); 
-	}
-*/
+        logger("Failed to change directory");
+        exit(EXIT_FAILURE); 
+    }
+    */
 
     // A second fork ensures the process cannot acquire a controlling terminal.
     if (pid_t pid = fork())
     {
-      if (pid > 0)
-      {
-        exit(0);
-      }
-      else
-      {
-        logger("Second fork failed");
-        return 1;
-      }
+        if (pid > 0)
+        {
+            exit(0);
+        }
+        else
+        {
+            logger("Second fork failed");
+            return 1;
+        }
     }
 
     // Close the standard streams. This decouples the daemon from the terminal
@@ -112,15 +112,15 @@ int daemonize() {
 
 int main() {
     logger("Starting daemon");
-	
-	daemonize();
-	
-	// Main process
-    while(true) {
-        process();
-        logger("Sleeping");
-        sleep(60);
+    
+    if ( daemonize() == 0 ) {
+        // Main process
+        while(true) {
+            process();
+            logger("Sleeping");
+            sleep(60);
+        }
     }
-	
-	logger("Exiting");
+    
+    logger("Exiting");
 }
